38373c3b157c715ae952945a3a575e0ac23967d3
JvMaia1 <vmals@hotmail.com>
2026-03-12 17:23:19 -0300

    finalização temporaria do projeto

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// 1. Seleciona os elementos do HTML pelos IDs corretos
+//1. Seleciona os elementos do HTML pelos IDs
 const fotoOriginal = document.getElementById("primeira-foto"); // ID da imagem na página
-const modal = document.getElementById("lightbox-modal");      // ID do fundo preto (modal)
-const fotoNoModal = document.getElementById("foto-modal");    // ID da imagem dentro do modal
+const modal = document.getElementById("lightbox-modal");       // ID do fundo preto (modal)
+const fotoAberta = document.getElementById("foto-modal");     // ID da imagem dentro do modal
+const galeriaContainer = document.querySelector('.galeria-container'); //endereço do local onde as fotos da galeria vão ficar
+const fotoPrincipal = document.getElementById("primeira-foto"); // imagem da foto principal que esta separada da galeria
 
-const fotoPrincipal = document.getElementById("primeira-foto");
+let fotosGaleria = []; //lista de fotos da galeria
+
+const totalFotosGaleria = 18; //quantidade de fotos da galeria
+
+//Evento que detecta a foto clicada à abre
 fotoPrincipal.addEventListener("click", () => {
-    configurarModal(fotoPrincipal.src);
+    abrirFotoClicada(fotoPrincipal.src);
 });
 
-function configurarModal(caminho) {
+//Função que coloca a foto clicada no modal e a expande
+function abrirFotoClicada(caminho) {
     modal.style.display = "flex";
-    fotoNoModal.src = caminho;
-    fotoNoModal.classList.add("foto-aumentada");
-}
-
-// 2. Evento para ABRIR o modal ao clicar na foto da Pitty
-fotoOriginal.addEventListener("click", function() {
-    modal.style.display = "flex";          // Mostra o fundo
-    fotoNoModal.src = fotoOriginal.src;    // Garante que a foto do modal é a mesma que foi clicada
-    fotoNoModal.classList.add("foto-aumentada");
-});
+    fotoAberta.src = caminho;
+    fotoAberta.classList.add("foto-aumentada");
+};
 
 // 3. Evento para FECHAR o modal ao clicar em qualquer lugar do fundo preto
 modal.addEventListener("click", function() {
@@ -27,19 +27,26 @@ modal.addEventListener("click", function() {
 });
 
 //Colocar as fotos na galeria
+for(let i = 1; i <= totalFotosGaleria; i++){
+    fotosGaleria.push(`fotos/foto-${i}.webp`)
+};
 
-let fotosGaleria = ["fotos/foto-1.jpg", "fotos/foto-2.jpg", "fotos/foto-3.jpg", "fotos/foto-4.jpg", "fotos/foto-5.jpg", "fotos/foto-6.jpg", "fotos/foto-7.jpg", "fotos/foto-8.jpg", "fotos/foto-9.jpg", "fotos/foto-10.jpg", "fotos/foto-11.jpg", "fotos/foto-12.jpg", "fotos/foto-13.jpg", "fotos/foto-14.jpg", "fotos/foto-15.jpg", "fotos/foto-16.jpg", "fotos/foto-17.jpg", "fotos/foto-18.jpg"]
-
-const galeriaContainer = document.querySelector('.galeria-container');
-
-
+//selecionando cada foto em um loop, adicionando a ao html e a dando a habilidade de ela abrir
 fotosGaleria.forEach(foto => {
+
+    //criando um elemento com a foto
     const img = document.createElement('img');
     img.src = foto;
     img.alt = "foto da pitty";
+    img.loading = "lazy";
     img.classList.add('foto-galeria');
-    img.addEventListener('click', () => {
-        configurarModal(foto); 
-    });
+
+    //colocando o novo elemento com a foto dentro da div '.galeria-container' no html
     galeriaContainer.appendChild(img);
+
+    //fazendo com que a foto 'ouça' clicks e abra quando clicada
+    img.addEventListener('click', () => {
+        abrirFotoClicada(foto); 
+    });
+    //
 });
